5a9ebcfce5198ca75545e87ea38286793e68fd6b
incursio16 <suhaimirizal@gmail.com>
2025-03-05 08:34:18 +0800

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <header>
+        <nav>
+            <NuxtLink to="/">Nuxt Dojo</NuxtLink>
+            <ul>
+                <li><NuxtLink to="/">Home</NuxtLink></li>
+                <li><NuxtLink to="/about">About</NuxtLink></li>
+                <li><NuxtLink to="/products">Products</NuxtLink></li>
+            </ul>
+        </nav>
+    </header>
+    <h2>Home</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis nam itaque laborum esse ullam fuga, ad quam officiis asperiores veritatis.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis nam itaque laborum esse ullam fuga, ad quam officiis asperiores veritatis.</p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    h2 {
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+
+    p {
+        margin: 20px 0;
+    }
+</style>
